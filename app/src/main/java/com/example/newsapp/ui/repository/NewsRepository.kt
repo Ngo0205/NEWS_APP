caf531e0de8a70b5cdbfa014ffc3bf3ad4d11325
Ngo Nguyen <ngonguyenvan910@gmail.com>
2022-02-21 10:48:43 +0700

    21/2

--- app/src/main/java/com/example/newsapp/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/ui/repository/NewsRepository.kt
@@ -1,8 +1,16 @@
 package com.example.newsapp.ui.repository
 
+import androidx.room.Query
+import com.example.newsapp.ui.api.RetrofitIntance
 import com.example.newsapp.ui.db.ArticleDatabase
 
 class NewsRepository(
     val db : ArticleDatabase
 ) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitIntance.api.getBreakingNews(countryCode, pageNumber)
+
+    suspend fun searchNews(searchQuery: String,pageNumber: Int) =
+        RetrofitIntance.api.searchForNews(searchQuery,pageNumber)
+
 }
